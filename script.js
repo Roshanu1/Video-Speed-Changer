ca8084cbf7ece34c23bb561592323a4df8978bca
Roshan Bhandari <roshanbhandari.716@gmail.com>
2020-06-10 11:01:25 +0545

    initial commit

---
+++ script.js
@@ -0,0 +1,37 @@
+$(document).on("change", ".file_multi_video", function(evt) {
+  var $source = $('#video_here');
+
+  $source[0].src = URL.createObjectURL(this.files[0]);
+ 
+  $source.parent()[0].load();
+
+});
+
+
+function showvid() {
+  console.log("This is clicked");
+  document.getElementById('showvideoplayer').style.display = "block";
+  document.getElementById('inputbar').style.display = 'none';
+  document.getElementById('speedChanger').style.display = "block";
+}
+
+var raw_video = document.getElementById('showvideoplayer');
+function change_speed() { 
+  var x = document.getElementById("select_speed").value;
+  console.log(x);
+  if (x == 'slow') {
+    raw_video.playbackRate = 0.5;
+  }
+  else if (x == 'two') {
+    raw_video.playbackRate = 2.0;
+  }
+  else if(x == 'four'){
+    raw_video.playbackRate = 4.0;
+  }
+  else if(x == 'eight'){
+    raw_video.playbackRate = 8.0;
+  }
+  else if(x == 'sixteen'){
+    raw_video.playbackRate = 16.0;
+  }
+} 
